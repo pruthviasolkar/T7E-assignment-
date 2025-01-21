a0d2f6abd78e20963d9a74f37e6b7b647dc2c54f
unknown <asolkarprutvhi7@gmail.com>
2025-01-22 01:44:42 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Slidebar from './components/Slidebar/slidebar'
+import Slidebar from './components/Slidebar/Slidebar.jsx'
 import Maindash from './components/Dashboard/Maindash'
 
 function App() {
